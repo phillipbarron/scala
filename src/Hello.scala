5528018714ea2f914e9fb56884f1087382c36c15
Phill Barron <phillipbarron242@gmail.com>
2019-09-28 21:10:16 +0100

    threaddie mercury

--- src/Hello.scala
+++ src/Hello.scala
@@ -1,15 +1,16 @@
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 
 object Hello extends App {
+  print(s"Number of cores: ${Runtime.getRuntime().availableProcessors()}\n")
   def printSomething(index: Int) : Future[Unit] = {
-    Future {
-      Thread.sleep(500)
-      println(s"the index is $index")
-    }
+    Await.result(Future {
+      Thread.sleep(10)
+      println(s"the index is $index, we are using thread ${Thread.currentThread().getId}")
+    }, Duration.Inf)
   }
   for(i <- 0 to 10) {
-    println(s"this is index $i")
     printSomething(i)
   }
 }
