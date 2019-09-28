0e9161815b76d9f891abdf84b28368daa71495b4
Phill Barron <phillipbarron242@gmail.com>
2019-09-28 20:07:58 +0100

    args

--- src/Hello.scala
+++ src/Hello.scala
@@ -1,5 +1,7 @@
-object Hello {
-  def main(args: Array[String]): Unit ={
+object Hello extends App {
+  if (args.size == 0) {
     println("hello")
+  } else {
+    println("all the things", args(0))
   }
 }
