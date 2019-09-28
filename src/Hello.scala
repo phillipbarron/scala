b47298d1fc003eb6d09258d47beecd9830ffa4d0
Phill Barron <phillipbarron242@gmail.com>
2019-09-28 20:56:27 +0100

    threaddie mercury

--- src/Hello.scala
+++ src/Hello.scala
@@ -1,7 +1,15 @@
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object Hello extends App {
-  if (args.size == 0) {
-    println("hello")
-  } else {
-    println("all the things", args(0))
+  def printSomething(index: Int) : Future[Unit] = {
+    Future {
+      Thread.sleep(500)
+      println(s"the index is $index")
+    }
+  }
+  for(i <- 0 to 10) {
+    println(s"this is index $i")
+    printSomething(i)
   }
 }
