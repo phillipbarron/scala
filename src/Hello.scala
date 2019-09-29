5cd7466349bd2effd971f834a26ee4479040a258
Phill Barron <phillipbarron242@gmail.com>
2019-09-29 08:42:18 +0100

    using all the threads

--- src/Hello.scala
+++ src/Hello.scala
@@ -1,3 +1,4 @@
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
@@ -5,12 +6,14 @@ import scala.concurrent.duration.Duration
 object Hello extends App {
   print(s"Number of cores: ${Runtime.getRuntime().availableProcessors()}\n")
   def printSomething(index: Int) : Future[Unit] = {
-    Await.result(Future {
+    Future {
       Thread.sleep(10)
       println(s"the index is $index, we are using thread ${Thread.currentThread().getId}")
-    }, Duration.Inf)
+    }
   }
+  var fruits = new ListBuffer[Future[Unit]]()
   for(i <- 0 to 10) {
-    printSomething(i)
+    fruits += printSomething(i)
   }
+  fruits.map(f => Await.result(f, Duration.Inf))
 }
