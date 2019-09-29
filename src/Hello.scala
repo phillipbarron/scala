590bc18841fab05a1cb00c97510961e86d9304b1
Phill Barron <phillipbarron242@gmail.com>
2019-09-29 12:11:01 +0100

    limited exection context

--- src/Hello.scala
+++ src/Hello.scala
@@ -1,16 +1,18 @@
+import java.util.concurrent.Executors
+
 import scala.collection.mutable.ListBuffer
-import scala.concurrent.{Await, Future}
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration.Duration
 
 object Hello extends App {
+  val executionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(100))
   print(s"Number of cores: ${Runtime.getRuntime().availableProcessors()}\n")
   def printSomething(index: Int) : Future[Unit] = {
     val startTime = System.nanoTime
     Future {
       Thread.sleep(100)
       println(s"the index is $index, we are using thread ${Thread.currentThread().getId}\nthe time elapsed for this call is ${(System.nanoTime() - startTime) / 1e9d}")
-    }
+    }(executionContext)
   }
   var fruits = new ListBuffer[Future[Unit]]()
   for(i <- 0 to 10) {
